e5db5140d6fc1a49c177c77e8795ef33df777811
Emil Popovic <mail@emilpopovic.me>
2025-11-09 23:43:12 +0100

    removed optional axi signals

--- rtl/simple_axi_master_wrapper.v
+++ rtl/simple_axi_master_wrapper.v
@@ -19,14 +19,6 @@ module simple_axi_master_wrapper(
     input  wire        m_axi_awready,
     output wire [31:0] m_axi_awaddr,
     output wire [2:0]  m_axi_awsize,
-    output wire [1:0]  m_axi_awburst,
-    output wire [3:0]  m_axi_awcache,
-    output wire [2:0]  m_axi_awprot,
-    output wire [7:0]  m_axi_awlen,
-    output wire        m_axi_awlock,
-    output wire [3:0]  m_axi_awqos,
-    output wire [3:0]  m_axi_awregion,
-    output wire [0:0]  m_axi_awid,
 
     output wire        m_axi_wvalid,
     input  wire        m_axi_wready,
@@ -37,27 +29,17 @@ module simple_axi_master_wrapper(
     input  wire        m_axi_bvalid,
     output wire        m_axi_bready,
     input  wire [1:0]  m_axi_bresp,
-    input  wire [0:0]  m_axi_bid,
 
     output wire        m_axi_arvalid,
     input  wire        m_axi_arready,
     output wire [31:0] m_axi_araddr,
     output wire [2:0]  m_axi_arsize,
-    output wire [1:0]  m_axi_arburst,
-    output wire [3:0]  m_axi_arcache,
-    output wire [2:0]  m_axi_arprot,
-    output wire [7:0]  m_axi_arlen,
-    output wire        m_axi_arlock,
-    output wire [3:0]  m_axi_arqos,
-    output wire [3:0]  m_axi_arregion,
-    output wire [0:0]  m_axi_arid,
 
     input  wire        m_axi_rvalid,
     output wire        m_axi_rready,
     input  wire        m_axi_rlast,
     input  wire [63:0] m_axi_rdata,
-    input  wire [1:0]  m_axi_rresp,
-    input  wire [0:0]  m_axi_rid
+    input  wire [1:0]  m_axi_rresp
 );
 
 simple_axi_master simple_axi_master (
@@ -77,14 +59,6 @@ simple_axi_master simple_axi_master (
     .m_axi_awready  (m_axi_awready),
     .m_axi_awaddr   (m_axi_awaddr),
     .m_axi_awsize   (m_axi_awsize),
-    .m_axi_awburst  (m_axi_awburst),
-    .m_axi_awcache  (m_axi_awcache),
-    .m_axi_awprot   (m_axi_awprot),
-    .m_axi_awlen    (m_axi_awlen),
-    .m_axi_awlock   (m_axi_awlock),
-    .m_axi_awqos    (m_axi_awqos),
-    .m_axi_awregion (m_axi_awregion),
-    .m_axi_awid     (m_axi_awid),
     .m_axi_wvalid   (m_axi_wvalid),
     .m_axi_wready   (m_axi_wready),
     .m_axi_wlast    (m_axi_wlast),
@@ -93,25 +67,15 @@ simple_axi_master simple_axi_master (
     .m_axi_bvalid   (m_axi_bvalid),
     .m_axi_bready   (m_axi_bready),
     .m_axi_bresp    (m_axi_bresp),
-    .m_axi_bid      (m_axi_bid),
     .m_axi_arvalid  (m_axi_arvalid),
     .m_axi_arready  (m_axi_arready),
     .m_axi_araddr   (m_axi_araddr),
     .m_axi_arsize   (m_axi_arsize),
-    .m_axi_arburst  (m_axi_arburst),
-    .m_axi_arcache  (m_axi_arcache),
-    .m_axi_arprot   (m_axi_arprot),
-    .m_axi_arlen    (m_axi_arlen),
-    .m_axi_arlock   (m_axi_arlock),
-    .m_axi_arqos    (m_axi_arqos),
-    .m_axi_arregion (m_axi_arregion),
-    .m_axi_arid     (m_axi_arid),
     .m_axi_rvalid   (m_axi_rvalid),
     .m_axi_rready   (m_axi_rready),
     .m_axi_rlast    (m_axi_rlast),
     .m_axi_rdata    (m_axi_rdata),
-    .m_axi_rresp    (m_axi_rresp),
-    .m_axi_rid      (m_axi_rid)
+    .m_axi_rresp    (m_axi_rresp)
 );
 
 endmodule
