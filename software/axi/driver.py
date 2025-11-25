a9431af6540db2d173657beb870c12419349c030
Emil Popovic <mail@emilpopovic.me>
2025-11-25 16:02:25 +0100

    importing driver from module

---
+++ software/axi/driver.py
@@ -0,0 +1,114 @@
+import time
+from pathlib import Path
+from pynq import Overlay
+
+from .enums import MemOp, MemSize, MemOpStatus, MemOpResult
+
+class SimpleAxiMasterDriver:
+    def __init__(self, overlay_path: Path) -> None:
+        self._ol = Overlay(overlay_path)
+
+        self.gpio_wdata = self._ol.gpio_wdata.channel1
+        self.gpio_rdata = self._ol.gpio_rdata.channel1
+        self.gpio_addr = self._ol.gpio_addr.channel1
+        self.gpio_ctrl = self._ol.gpio_ctrl.channel1
+        self.gpio_status = self._ol.gpio_ctrl.channel2
+
+    @property
+    def rdata(self) -> int:
+        return self.gpio_rdata.read()
+    
+    @property
+    def status(self) -> int:
+        return self.gpio_status.read()
+    
+    @property
+    def wdata(self) -> int:
+        return self.gpio_wdata.read()
+    
+    @wdata.setter
+    def wdata(self, data: int) -> None:
+        return self.gpio_wdata.write(data)
+    
+    @property
+    def addr(self) -> int:
+        return self.gpio_addr.read()
+
+    @addr.setter
+    def addr(self, addr: int) -> None:
+        return self.gpio_addr.write(addr)
+    
+    @property
+    def ctrl(self) -> int:
+        return self.gpio_ctrl.read()
+
+    @ctrl.setter
+    def ctrl(self, ctrl: int) -> None:
+        return self.gpio_ctrl.write(ctrl)
+
+    @staticmethod
+    def __make_ctrl(clear: bool = False, cmd: MemOp = MemOp.IDLE, size: MemSize = MemSize.BYTE) -> int:
+        # [5:5]=Clear, [4:3]=RW, [2:0]=Size, start transaction
+        return (int(clear) << 5) | ((cmd & 0x3) << 3) | (size & 0x7)
+
+    def __execute(self, cmd: MemOp, size: MemSize, addr: int, data: int = 0) -> MemOpResult:
+        """Execute raw transfer"""
+        self.addr = addr
+
+        if cmd == MemOp.WRITE:
+            self.wdata = data & 0xFFFF_FFFF
+
+        self.ctrl = self.__make_ctrl(False, cmd, size)
+
+        status = 0
+        for _ in range(100):
+            status = self.status
+            done = (status >> 1) & 0x1
+            if done:
+                break
+            time.sleep(0.001)
+
+        # Return to idle
+        self.ctrl = self.__make_ctrl()
+
+        # Read result
+        result_data = 0
+        if cmd == MemOp.READ:
+            result_data = self.rdata
+
+        return MemOpResult(MemOpStatus.from_reg(status), result_data)
+    
+    def clear(self) -> None:
+        self.ctrl = self.__make_ctrl(clear=True)
+        time.sleep(0.01)
+        self.ctrl = self.__make_ctrl()
+
+    def read(self, size: MemSize, addr: int) -> MemOpResult:
+        return self.__execute(MemOp.READ, size, addr)
+    
+    def read_byte(self, addr: int) -> MemOpResult:
+        return self.__execute(MemOp.READ, MemSize.BYTE, addr)
+    
+    def read_half(self, addr: int) -> MemOpResult:
+        return self.__execute(MemOp.READ, MemSize.HALF, addr)
+    
+    def read_word(self, addr: int) -> MemOpResult:
+        return self.__execute(MemOp.READ, MemSize.WORD, addr)
+    
+    def read_dword(self, addr: int) -> MemOpResult:
+        return self.__execute(MemOp.READ, MemSize.DWORD, addr)
+
+    def write(self, size: MemSize, addr: int, data: int) -> MemOpStatus:
+        return self.__execute(MemOp.WRITE, size, addr, data).status
+    
+    def write_byte(self, addr: int, data: int) -> MemOpStatus:
+        return self.__execute(MemOp.WRITE, MemSize.BYTE, addr, data).status
+    
+    def write_half(self, addr: int, data: int) -> MemOpStatus:
+        return self.__execute(MemOp.WRITE, MemSize.HALF, addr, data).status
+    
+    def write_word(self, addr: int, data: int) -> MemOpStatus:
+        return self.__execute(MemOp.WRITE, MemSize.WORD, addr, data).status
+    
+    def write_dword(self, addr: int, data: int) -> MemOpStatus:
+        return self.__execute(MemOp.WRITE, MemSize.DWORD, addr, data).status
---
+++ software/axi/enums.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+class MemOp(int, Enum):
+    IDLE = 0
+    WRITE = 1
+    READ = 2
+
+class MemSize(int, Enum):
+    BYTE = 0
+    HALF = 1
+    WORD = 2
+    DWORD = 3
+
+    @staticmethod
+    def from_bit_count(bit_count: int) -> 'MemSize':
+        bits_to_size = {
+            8: MemSize.BYTE,
+            16: MemSize.HALF,
+            32: MemSize.WORD,
+            64: MemSize.DWORD,
+        }
+        return bits_to_size[bit_count]
+
+class MemOpStatus(str, Enum):
+    NONE = 'none'
+    DONE = 'done'
+    WAIT = 'wait'
+    ERROR = 'error'
+    INVALID = 'invalid'
+
+    @staticmethod
+    def from_reg(status_reg: int) -> 'MemOpStatus':
+        if (status_reg >> 3) & 0x1:
+            return MemOpStatus.INVALID
+        elif (status_reg >> 2) & 0x1:
+            return MemOpStatus.ERROR
+        elif (status_reg >> 1) & 0x1:
+            return MemOpStatus.DONE
+        elif status_reg & 0x1:
+            return MemOpStatus.WAIT
+        else:
+            return MemOpStatus.NONE
+
+class MemOpResult:
+    def __init__(self, status: MemOpStatus, data: int) -> None:
+        self.status = status
+        self.data = data
